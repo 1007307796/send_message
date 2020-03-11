515462a73f123f97930c9affa411be263492edc1
git <1007307796@qq.com>
2020-03-11 16:34:09 +0800

    fix bug and add loveword

--- zhenzismsclient.py
+++ zhenzismsclient.py
@@ -8,13 +8,13 @@ class ZhenziSmsClient(object):
 		self.appSecret = appSecret
 
 	def send(self, params):
-		data = params;
-		data['appId'] = self.appId;
-		data['appSecret'] = self.appSecret;
-		requests.packages.urllib3.disable_warnings(InsecureRequestWarning);
-		response = requests.post(self.apiUrl+'/sms/send.do', data=data, verify=False);
-		result = str(response.content,'utf-8');
-		return result;
+		data = params
+		data['appId'] = self.appId
+		data['appSecret'] = self.appSecret
+		requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+		response = requests.post(self.apiUrl+'/sms/send.do', data=data, verify=False)
+		result = str(response.content,'utf-8')
+		return result
 
 
 	def balance(self):
@@ -22,10 +22,10 @@ class ZhenziSmsClient(object):
 		    'appId': self.appId,
 		    'appSecret': self.appSecret
 		}
-		requests.packages.urllib3.disable_warnings(InsecureRequestWarning);
-		response = requests.post(self.apiUrl+'/account/balance.do', data=data, verify=False);
-		result = str(response.content,'utf-8');
-		return result;
+		requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+		response = requests.post(self.apiUrl+'/account/balance.do', data=data, verify=False)
+		result = str(response.content,'utf-8')
+		return result
 
 	def findSmsByMessageId(self, messageId):
 		data = {
@@ -33,7 +33,7 @@ class ZhenziSmsClient(object):
 		    'appSecret': self.appSecret,
 		    'messageId': messageId
 		}
-		requests.packages.urllib3.disable_warnings(InsecureRequestWarning);
-		response = requests.post(self.apiUrl+'/smslog/findSmsByMessageId.do', data=data, verify=False);
-		result = str(response.content,'utf-8');
-		return result;
+		requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+		response = requests.post(self.apiUrl+'/smslog/findSmsByMessageId.do', data=data, verify=False)
+		result = str(response.content,'utf-8')
+		return result
